ddbda49284b65b026acea0e768eb265b43bd87a4
Brandon Carr <BCARR85@live.com>
2023-10-29 09:41:54 -0700

    Added Api skeleton

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _dbContext;
+    public UsersController(DataContext dbContext)
+    {
+        _dbContext = dbContext;
+
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _dbContext.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id:int}")] // /api/users/2
+    public async Task<ActionResult<AppUser>> GetUserById([FromRoute] int id)
+    {
+        var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return user;
+    }
+
+}
